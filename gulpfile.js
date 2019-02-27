4824c00c6e03ef18e426bbd5389bfcc2a508c095
Katarzyna Moskała <kasia.moskala.20.03@gmail.com>
2019-02-27 12:55:12 +0100

    fix bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,27 @@
 
 var gulp = require('gulp')
-  , minifyHtml = require("gulp-minify-html")
   , minifyCss = require("gulp-minify-css")
   , uglify = require("gulp-uglify")
   , sass = require("gulp-sass");
 
 sass.compiler = require('node-sass');
-// task minify html
-gulp.task('html', function () {
-  return gulp.src('src/*.html') // path to your files
-    .pipe(minifyHtml())
-    .pipe(gulp.dest('dist/'));
-});
-
 
 // task uglify js
 gulp.task('js', function () {
- return gulp.src('src/js/*.js') // path to your files
+ return gulp.src('js/*.js') // path to your files
     .pipe(uglify())
-    .pipe(gulp.dest('src/js.min'));
+    .pipe(gulp.dest('js.min'));
 });
 // task sass to css
 gulp.task('sass', function () {
-  return gulp.src('src/scss/*.scss') // path to your file
+  return gulp.src('scss/*.scss') // path to your file
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCss())
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./src/scss/*.scss', ['sass']);
-  gulp.watch('./src/js/*.js', ['js']);
-  gulp.watch('./*.html', ['html']);
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('js/*.js', ['js']);
   // Other watchers
 });
--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-$(document).ready(function () {
-  var screen_height = $(window).height();
-  var headers_height = $('.headers_container').height();
+function init() {
+  var screen_height     = $(window).height(),
+      screen_width      = $(window).width(),
+      headers_height    = $('.headers_container').height();
+    
+  if(!isMobile()) {
+	$('#main_banner, #footer').height(screen_height);
+	$('.headers_container').css('padding', (screen_height - headers_height) / 2 + 'px 0');
+  } else {
+	$('#main_banner, #footer').css('height', 'inherit');
+	$('.headers_container').css('padding', '150px 0');
+	
+  }
   
-  $('#main_banner, #about_section').height(screen_height);
-  $('')
-  $('.headers_container').css('padding', (screen_height-headers_height)/2+'px 0');
-
+}
+function isMobile() {
+  if($(window).width() < 768 ) {
+	return true;
+  } else {
+	return false;
+  }
+}
+$(window).resize(function () {
+  init();
+  
+});
+$(document).ready(function () {
+  init();
   $('#main_banner .icon_container').click(function () {
     $('html, body').animate({
 	  scrollTop: $("#main_gallery").offset().top
@@ -16,7 +36,4 @@ $(document).ready(function () {
 	  scrollTop: $("#main_gallery").offset().top
 	}, 1500);
   });
-  
-  
-  
 });
